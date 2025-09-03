5cd40f16bb8d45d251bd2f7f36e24f35d5c810c1
Godfather0369 <xxxalok4444@gmail.com>
2025-09-03 15:24:06 +0000

    ghghf

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,26 +1,15 @@
 #include<iostream>
-
 using namespace std;
-
 int findKey();
-
 int main(){
-
-
     int ans = findKey();
-   
     if(ans ==-1){
         cout<<"Element Not Found ";
     }else{
         cout<<"Element  Found  at "<<ans+1;
-
     }
-
 }
-
-
 int findKey(){
-
     int i; 
     int arr[5] = {33,45,12,8,20};
     int k = 12;
